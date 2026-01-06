916bf5483d6dc4d196104db9bf9f734dba0f4a68
couto06 <1211497@isep.ipp.pt>
2026-01-06 17:53:24 +0000

    Dim Country

--- ssispkg/ARMDD_ETL/SQL_Scripts/DataMart/CreateDimCountry.sql
+++ ssispkg/ARMDD_ETL/SQL_Scripts/DataMart/CreateDimCountry.sql
@@ -1,13 +1,15 @@
 IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'DimCountry')
 BEGIN
-    CREATE TABLE dbo.DimCountry (
-    CountryID      INT           NOT NULL,
-    CountryName    VARCHAR(100)  NULL,
-    CountryCode    VARCHAR(10)   NULL,
-    VATRate        DECIMAL(5,2)  NULL,
-    IsEuroZone     BIT           NULL,
-    CreateDate     DATE          NULL,
-    LastUpdateDate DATE          NULL,
-    CONSTRAINT PK_DimCountry PRIMARY KEY (CountryID)
+    CREATE TABLE dbo.DimCountry
+    (
+        CountryKey INT IDENTITY(1,1) NOT NULL,
+        CountryID INT NOT NULL,
+        CountryName VARCHAR(100) NULL,
+        CountryCode VARCHAR(10) NULL,
+        VATRate DECIMAL(5,2) NULL,
+        IsEuroZone BIT NULL,
+        CreateDate DATE NULL,
+        LastUpdateDate DATE NULL,
+        CONSTRAINT PK_DimCountry_Key PRIMARY KEY CLUSTERED (CountryKey ASC)
     );
 END
