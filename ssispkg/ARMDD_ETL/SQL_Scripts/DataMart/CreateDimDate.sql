8a8a5cad7838bc45c1531177bf62f07d9d2d2b86
couto06 <1211497@isep.ipp.pt>
2026-01-06 16:42:55 +0000

    Change in DimDate

--- ssispkg/ARMDD_ETL/SQL_Scripts/DataMart/CreateDimDate.sql
+++ ssispkg/ARMDD_ETL/SQL_Scripts/DataMart/CreateDimDate.sql
@@ -1,21 +1,24 @@
 IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'DimDate')
 BEGIN
-    CREATE TABLE [dbo].[DimDate](
-        [DateKey] [int] IDENTITY(1,1)    NOT NULL,
-        [FullDate]         [date]        NULL,
-        [Year]             [int]         NULL,
-        [Month]            [tinyint]     NULL,
-        [DayNumberOfYear]  [int]         NULL,
-        [DayNumberOfMonth] [tinyint]     NULL,
-        [DayNumberOfWeek]  [tinyint]     NULL,
-        [Week]             [tinyint]     NULL,
-        [DayOfWeek]        [nvarchar](10)        NULL,
-        [Weekend]          [nvarchar](10)  NULL,
-        [MonthName]        [nvarchar](10) NULL,
-        [Quarter]          [tinyint]     NULL,
-        [Semester]         [tinyint]     NULL,
-        [Trimester]        [tinyint]     NULL,
-        [Season]           [nvarchar](10)     NULL,
-        CONSTRAINT [PK_DimDate] PRIMARY KEY CLUSTERED ([DateKey] ASC)
+    CREATE TABLE dbo.DimDate
+    (
+        DateKey INT IDENTITY(1,1) NOT NULL,
+        FullDate DATE NULL,
+        [Year] INT NULL,
+        [Month] TINYINT NULL,
+        DayNumberOfYear INT NULL,
+        DayNumberOfMonth TINYINT NULL,
+        DayNumberOfWeek TINYINT NULL,
+        [Week] TINYINT NULL,
+        DayOfWeek NVARCHAR(20) NULL,       
+        Weekend NVARCHAR(10) NULL,         
+        MonthName NVARCHAR(20) NULL,       
+        Quarter TINYINT NULL,              
+        Semester TINYINT NULL,             
+        Trimester TINYINT NULL,            
+        Season NVARCHAR(20) NULL,          
+        CONSTRAINT PK_DimDate
+            PRIMARY KEY CLUSTERED (DateKey)
     );
-END
+END;
+GO
