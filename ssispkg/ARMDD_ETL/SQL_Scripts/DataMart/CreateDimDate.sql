03323fad887b24961a671df957d380305cd2b145
couto06 <1211497@isep.ipp.pt>
2026-01-06 15:58:01 +0000

    Adjust in CreateDimDate

--- ssispkg/ARMDD_ETL/SQL_Scripts/DataMart/CreateDimDate.sql
+++ ssispkg/ARMDD_ETL/SQL_Scripts/DataMart/CreateDimDate.sql
@@ -1,21 +1,36 @@
 IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'DimDate')
 BEGIN
-    CREATE TABLE [dbo].[DimDate](
-        [DateKey] [int] IDENTITY(1,1)    NOT NULL,
-        [FullDate]         [date]        NULL,
-        [Year]             [int]         NULL,
-        [Month]            [tinyint]     NULL,
-        [DayNumberOfYear]  [int]         NULL,
-        [DayNumberOfMonth] [tinyint]     NULL,
-        [DayNumberOfWeek]  [tinyint]     NULL,
-        [Week]             [tinyint]     NULL,
-        [DayOfWeek]        [nvarchar](10)        NULL,
-        [Weekend]          [nvarchar](10)  NULL,
-        [MonthName]        [nvarchar](10) NULL,
-        [Quarter]          [tinyint]     NULL,
-        [Semester]         [tinyint]     NULL,
-        [Trimester]        [tinyint]     NULL,
-        [Season]           [nvarchar](10)     NULL,
-        CONSTRAINT [PK_DimDate] PRIMARY KEY CLUSTERED ([DateKey] ASC)
+    CREATE TABLE dbo.DimDate
+    (
+        DateKey INT IDENTITY(1,1) NOT NULL,
+        FullDate DATE NOT NULL,
+        [Year] INT,
+        YearName NVARCHAR(20),
+        HalfYear TINYINT,
+        HalfYearName NVARCHAR(30),
+        Quarter TINYINT,
+        QuarterName NVARCHAR(30),
+        Trimester TINYINT,
+        TrimesterName NVARCHAR(30),
+        [Month] TINYINT,
+        MonthName NVARCHAR(20),
+        TenDays TINYINT,
+        TenDaysName NVARCHAR(30),
+        [Week] TINYINT,
+        WeekName NVARCHAR(30),
+        DayOfYear INT,
+        DayOfMonth TINYINT,
+        DayOfWeek TINYINT,
+        DayOfWeekName NVARCHAR(20),
+        Weekend NVARCHAR(10),
+        ISOYear INT,
+        ISOYearName NVARCHAR(30),
+        ISOWeek TINYINT,
+        ISOWeekName NVARCHAR(30),
+        ISODayOfWeek TINYINT,
+        ISODayOfWeekName NVARCHAR(20),
+        Season NVARCHAR(20),
+        CONSTRAINT PK_DimDate PRIMARY KEY CLUSTERED (DateKey)
     );
 END
+GO
